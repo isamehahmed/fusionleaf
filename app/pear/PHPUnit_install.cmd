57873face8bc364033039221ed320e1024f1d3a1
Joseph Spurrier <joe@pcprecision.com>
2012-12-11 17:49:57 -0500

    Change PHPUnit script to use channel pear.symfony.com instead of pear.symfony-project.com

--- app/pear/PHPUnit_install.cmd
+++ app/pear/PHPUnit_install.cmd
@@ -6,7 +6,7 @@ PAUSE
 cmd /C pear update-channels
 cmd /C pear upgrade
 cmd /C pear channel-discover pear.phpunit.de
-cmd /C pear channel-discover pear.symfony-project.com
+cmd /C pear channel-discover pear.symfony.com
 cmd /C pear clear-cache
 cmd /C pear install --alldeps phpunit/PHPUnit
 
